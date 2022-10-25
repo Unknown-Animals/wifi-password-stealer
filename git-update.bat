8e30196a3c5ecf1a463e102412f226615ed2ba6c
Todo Lodo <me@todolodo.xyz>
2022-10-25 20:29:01 +0530

    Update, date - Tue 10/25/2022, time - 20:29:00.91
    Co-authored-by: Unknown-Animals <me@unan.todolodo.xyz>

--- git-update.bat
+++ git-update.bat
@@ -1,5 +1,5 @@
 git pull
 git add .
-git commit -S -m "Update, date - %DATE%, time - %TIME%" --trailer "Co-authored-by: Unknown-Animal <me@unan.todolodo.xyz>"
+git commit -S -m "Update, date - %DATE%, time - %TIME%" --trailer "Co-authored-by: Unknown-Animals <me@unan.todolodo.xyz>"
 git push -u origin main
 pause
